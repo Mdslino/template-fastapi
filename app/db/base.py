1b7cade6f2a2d0b1f0109c8542338abf87b2aa41
Marcelo Lino <mdslino@gmail.com>
2024-09-02 19:37:45 -0300

    Going back to SQLAlchemy

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,3 @@
 from app.db.base_class import Base  # noqa
 
-__all__ = ("Base",)
+__all__ = ('Base',)
--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,11 +1,26 @@
-from datetime import UTC, datetime
+import uuid
+from datetime import datetime
 
-from sqlmodel import SQLModel, Field
-
-
-class Base(SQLModel):
-    id: int = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default=datetime.now(tz=UTC), sa_column_kwargs={"db_default": "now()"})
-    updated_at: datetime = Field(default=datetime.now(tz=UTC), sa_column_kwargs={"db_default": "now()"})
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+class BaseModel(Base):
+    __abstract__ = True
+    id: Mapped[int] = mapped_column(primary_key=True)
+    external_id: Mapped[uuid.UUID] = mapped_column(
+        index=True, unique=True, default_factory=uuid.uuid4
+    )
+    created_at: Mapped[datetime] = mapped_column(default_factory=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(
+        default_factory=datetime.now, onupdate=datetime.now
+    )
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {self.id}>'
+
+    def __str__(self):
+        return f'{self.__class__.__name__} {self.id}'
