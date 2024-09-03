f2fe48ed70c5478e28a3bac615490c1276d0e50d
Marcelo Lino <mdslino@gmail.com>
2024-09-03 11:40:57 -0300

    Add more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import contextlib
 
 import pytest
 from fastapi.testclient import TestClient
+from gotrue import UserResponse
 from sqlalchemy import text
 from sqlalchemy_utils import create_database, database_exists
 
@@ -9,6 +10,12 @@ from app.db.base import Base
 from app.main import create_app
 from tests.factories.session import Session, engine
 
+@pytest.fixture(scope='module')
+def vcr_config():
+    return {
+        'filter_headers': ['authorization', 'apikey'],
+        'filter_query_parameters': ['apikey'],
+    }
 
 @pytest.fixture
 def app():
@@ -63,3 +70,49 @@ def db():
 @pytest.fixture
 def anyio_backend():
     return 'asyncio'
+
+@pytest.fixture
+def supabase_user():
+    return UserResponse(**{
+        'user': {
+            'id': '1b278ccb-9ebb-4a96-b7ef-576b57a18931',
+            'app_metadata': {'provider': 'email', 'providers': ['email']},
+            'user_metadata': {},
+            'aud': 'authenticated',
+            'confirmation_sent_at': None,
+            'recovery_sent_at': None,
+            'email_change_sent_at': None,
+            'new_email': None,
+            'new_phone': None,
+            'invited_at': None,
+            'action_link': None,
+            'email': 'email@domain.com',
+            'phone': '',
+            'created_at': '2024-09-02T22:32:00.411824Z',
+            'confirmed_at': '2024-09-02T22:32:00.418600Z',
+            'email_confirmed_at': '2024-09-02T22:32:00.418600Z',
+            'phone_confirmed_at': None,
+            'last_sign_in_at': '2024-09-02T22:36:58.736282Z',
+            'role': 'authenticated',
+            'updated_at': '2024-09-02T22:36:58.737969Z',
+            'identities': [
+                {
+                    'id': '1b278ccb-9ebb-4a96-b7ef-576b57a18931',
+                    'identity_id': '320dfa25-ad88-48a7-892a-079540688613',
+                    'user_id': '1b278ccb-9ebb-4a96-b7ef-576b57a18931',
+                    'identity_data': {
+                        'email': 'email@domain.com',
+                        'email_verified': True,
+                        'phone_verified': False,
+                        'sub': '1b278ccb-9ebb-4a96-b7ef-576b57a18931',
+                    },
+                    'provider': 'email',
+                    'created_at': '2024-09-02T22:32:00.415439Z',
+                    'last_sign_in_at': '2024-09-02T22:32:00.415382Z',
+                    'updated_at': '2024-09-02T22:32:00.415439Z',
+                }
+            ],
+            'is_anonymous': False,
+            'factors': None,
+        }
+    })
--- tests/endpoints/test_healthcheck.py
+++ tests/endpoints/test_healthcheck.py
@@ -1,7 +1,22 @@
 from unittest.mock import ANY
+from unittest import mock
+
+import pytest
 
 
 def test_healthcheck(client):
     response = client.get('/healthcheck')
     assert response.status_code == 200
     assert response.json() == {'app': 'ok', 'db': 'ok', 'version': ANY}
+
+
+@pytest.mark.vcr()
+def test_authenticated_healthcheck(client):
+    response = client.get(
+        '/auth-healthcheck',
+        headers={
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IlZCeTZzSzVXdTNoelhFN04iLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3JjZXd2ZXp2cWh5a2t6a2N1dWlrLnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiIxYjI3OGNjYi05ZWJiLTRhOTYtYjdlZi01NzZiNTdhMTg5MzEiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzI1Mzc3MzMzLCJpYXQiOjE3MjUzNzM3MzMsImVtYWlsIjoibWRzbGlub0BnbWFpbC5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7fSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJwYXNzd29yZCIsInRpbWVzdGFtcCI6MTcyNTM3MzczM31dLCJzZXNzaW9uX2lkIjoiM2VhZDQwMGYtYWY5YS00YzdkLTk0ZGQtM2VlOGRjNDdlYmE3IiwiaXNfYW5vbnltb3VzIjpmYWxzZX0.wXVsiL4lFiMGraX4je_G974zpr3bXIPv2__SotMo5D0'
+        },
+    )
+
+    assert response.status_code == 200
