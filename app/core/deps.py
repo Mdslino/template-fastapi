f8023b052b972ab8ed6ed932e6eb62df0fbc1752
Marcelo Lino <mdslino@gmail.com>
2024-11-02 15:55:06 -0300

    Remover código não utilizado e ajustar formatação em arquivos de teste

--- app/core/deps.py
+++ app/core/deps.py
@@ -10,6 +10,7 @@ from app.db import engine
 logger = structlog.get_logger(__name__)
 security = HTTPBearer(auto_error=False)
 
+
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
--- tests/endpoints/test_healthcheck.py
+++ tests/endpoints/test_healthcheck.py
@@ -8,13 +8,3 @@ def test_healthcheck(client):
     response = client.get('/healthcheck')
     assert response.status_code == 200
     assert response.json() == {'app': 'ok', 'db': 'ok', 'version': ANY}
-
-
-@pytest.mark.vcr()
-def test_authenticated_healthcheck(client):
-    response = client.get(
-        '/auth-healthcheck',
-        headers={'Authorization': 'Bearer anykey'},
-    )
-
-    assert response.status_code == 200
